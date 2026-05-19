3a21791c9e17c37f24efeb054641cd934b44f087
Osarugue Enehizena <nehirugue@gmail.com>
2026-05-19 01:35:49 +0100

    feat: add subscription routes and implement missing user routes

---
+++ routes/subscription.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', (req, res) => res.send({message: 'All subscriptions'}));
+
+subscriptionRouter.get('/:id', (req, res) => res.send({message: 'Subscription by id'}));
+
+subscriptionRouter.post('/', (req, res) => res.send({message: 'Subscription created'}));
+
+subscriptionRouter.put('/:id', (req, res) => res.send({message: 'Subscription updated'}));
+
+subscriptionRouter.delete('/:id', (req, res) => res.send({message: 'Subscription deleted'}));
+
+
+export default subscriptionRouter;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,13 @@ import { Router } from "express";
 const userRouter = Router();
 
 userRouter.get('/', (req, res) => res.send({message: 'All users'}));
+
 userRouter.get('/:id', (req, res) => res.send({message: 'User by id'}));
+
+userRouter.post('/', (req, res) => res.send({message: 'User created'}));
+
 userRouter.put('/:id', (req, res) => res.send({message: 'User updated'}));
+
 userRouter.delete('/:id', (req, res) => res.send({message: 'User deleted'}));
 
 
